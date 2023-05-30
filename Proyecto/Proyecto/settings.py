acea092892e8df09e294b27f2bf609742765f21c
JGer04 <juarezalex282@gmail.com>
2023-05-30 09:07:56 -0600

    modificación de la pantalla admin

--- Proyecto/Proyecto/settings.py
+++ Proyecto/Proyecto/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
     # Aplicaciones externas
     'bootstrap5',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
