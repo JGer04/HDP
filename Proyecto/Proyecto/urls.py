15cca1035eac39eef9f0e48d67abb5ee396640eb
JGer04 <juarezalex282@gmail.com>
2023-05-30 15:34:10 -0600

    Se agrego la pagina inicio

--- Proyecto/Proyecto/urls.py
+++ Proyecto/Proyecto/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include ('aplicaciones.index.urls')),
+    path('', include ('aplicaciones.trabajo.urls')),
+    path('', include ('aplicaciones.educacion.urls')),
 ]
