586703015eb1d3a89a3aa51d78581668d65f199b
JGer04 <134817299+JGer04@users.noreply.github.com>
2023-06-02 11:59:23 -0600

    modificación de vista trabajo

--- Proyecto/aplicaciones/trabajo/urls.py
+++ Proyecto/aplicaciones/trabajo/urls.py
@@ -1,7 +1,31 @@
 from django.urls import path
 from . import views
 
+#app_name = 'trabajo_app'
 urlpatterns = [
     path('trabajo/trabajo', views.tablas, name='trabajo'),
-    path('trabajo/modificar', views.crearTabla.as_view(), name='modificar'),
+    #---------------- URLS DEL CRUD TABLA NIVEL -------------------------------------------------------
+    path('trabajo/tabla-nivel/crear', views.crearTablaN.as_view(), name='Ncrear'),
+    path('trabajo/tabla-nivel/modificar/<pk>/', views.modificarTablaN.as_view(), name='Nmodificar'),
+    path('trabajo/tabla-nivel/eliminar/<pk>/', views.eliminarTablaN.as_view(), name='Neliminar'),
+
+    #----------------- URLS DEL CRUD ENCUESTA UBBANA -------------------------------------------------
+    path('trabajo/encuesta-urbana/crear', views.crearTablaUR.as_view(), name='URcrear'),
+    path('trabajo/encuesta-urbana/modificar/<pk>/', views.modificarTablaUR.as_view(), name='URmodificar'),
+    path('trabajo/encuesta-urbana/eliminar/<pk>/', views.eliminarTablaUR.as_view(), name='UReliminar'),
+
+    #----------------- URLS DEL CRUD ENCUESTA RURAL -------------------------------------------------
+    path('trabajo/encuesta-rural/crear', views.crearTablaRU.as_view(), name='RUcrear'),
+    path('trabajo/encuesta-rural/modificar/<pk>/', views.modificarTablaRU.as_view(), name='RUmodificar'),
+    path('trabajo/encuesta-rural/eliminar/<pk>/', views.eliminarTablaRU.as_view(), name='RUeliminar'),
+
+     #----------------- URLS DEL CRUD TIPO DISCAPACIDAD -------------------------------------------------
+    path('trabajo/tipo-disc/crear', views.crearTablaDI.as_view(), name='DIcrear'),
+    path('trabajo/tipo-disc/modificar/<pk>/', views.modificarTablaDI.as_view(), name='DImodificar'),
+    path('trabajo/tipo-disc/eliminar/<pk>/', views.eliminarTablaDI.as_view(), name='DIeliminar'),
+
+     #----------------- URLS DEL CRUD CATEGORIA -------------------------------------------------
+    path('trabajo/tabla-categoria/crear', views.crearTablaCA.as_view(), name='CAcrear'),
+    path('trabajo/tabla-categoria/modificar/<pk>/', views.modificarTablaCA.as_view(), name='CAmodificar'),
+    path('trabajo/tabla-categoria/eliminar/<pk>/', views.eliminarTablaCA.as_view(), name='CAeliminar'),
 ]
--- Proyecto/aplicaciones/trabajo/views.py
+++ Proyecto/aplicaciones/trabajo/views.py
@@ -23,10 +23,112 @@ def tablas(request):
         'analisis': analisiss,
         'tnivel':tnivel,
     })
-
-class crearTabla(CreateView):
-    template_name = 'trabajo/modificar.html'
+# -------------------- CRUD DE TABLA NIVELES ------------------------------
+class crearTablaN(CreateView):
+    template_name = 'trabajo/tabla-nivel/crear.html'
     model = tablaNivel
     fields = ['nivel','total','hombre','mujer']
-    success_url = reverse_lazy('trabajo/trabajo.html')
+    success_url = reverse_lazy('trabajo_app:trabajo')
+
+class modificarTablaN(UpdateView):
+    template_name = 'trabajo/tabla-nivel/modificar.html'
+    model = tablaNivel
+    fields = ['nivel','total','hombre','mujer']
+    success_message = "El registro %(nombre)s fue mofificado exitosamente"
+    success_url = reverse_lazy('trabajo_app:trabajo')
+
+class eliminarTablaN(DeleteView):
+    template_name = 'trabajo/tabla-nivel/eliminar.html'
+    model = tablaNivel
+    fields = ['nivel','total','hombre','mujer']
+    success_message = "El registro %(nombre)s fue eliminado exitosamente"
+    success_url = reverse_lazy('trabajo_app:trabajo')
+
+# -------------------- CRUD DE TABLA ENC. URBANA ---------------------------
+
+class crearTablaUR(CreateView):
+    template_name = 'trabajo/encuesta-urbana/crear.html'
+    model = tablaEncuestaUrbana
+    fields = ['grupoEdad','total','hombre','mujer']
+    success_url = reverse_lazy('trabajo_app:trabajo')
+
+class modificarTablaUR(UpdateView):
+    template_name = 'trabajo/encuesta-urbana/modificar.html'
+    model = tablaEncuestaUrbana
+    fields = ['grupoEdad','total','hombre','mujer']
+    success_message = "El registro %(nombre)s fue mofificado exitosamente"
+    success_url = reverse_lazy('trabajo_app:trabajo')
+
+class eliminarTablaUR(DeleteView):
+    template_name = 'trabajo/encuesta-urbana/eliminar.html'
+    model = tablaEncuestaUrbana
+    fields = ['grupoEdad','total','hombre','mujer']
+    success_message = "El registro %(nombre)s fue eliminado exitosamente"
+    success_url = reverse_lazy('trabajo_app:trabajo')
+
+# -------------------- CRUD DE TABLA ENC. RURAL ---------------------------
+
+class crearTablaRU(CreateView):
+    template_name = 'trabajo/encuesta-rural/crear.html'
+    model = tablaEncuestaRural
+    fields = ['grupoEdad','total','hombre','mujer']
+    success_url = reverse_lazy('trabajo_app:trabajo')
+
+class modificarTablaRU(UpdateView):
+    template_name = 'trabajo/encuesta-rural/modificar.html'
+    model = tablaEncuestaRural
+    fields = ['grupoEdad','total','hombre','mujer']
+    success_message = "El registro %(nombre)s fue mofificado exitosamente"
+    success_url = reverse_lazy('trabajo_app:trabajo')
+
+class eliminarTablaRU(DeleteView):
+    template_name = 'trabajo/encuesta-rural/eliminar.html'
+    model = tablaEncuestaRural
+    fields = ['grupoEdad','total','hombre','mujer']
+    success_message = "El registro %(nombre)s fue eliminado exitosamente"
+    success_url = reverse_lazy('trabajo_app:trabajo')
+
+# -------------------- CRUD DE TABLA TIPO DISCAPACIDAD ---------------------------
+
+class crearTablaDI(CreateView):
+    template_name = 'trabajo/tipo-disc/crear.html'
+    model = tablaTipoDiscapacidad
+    fields = ['discapacidad','numero_Ocupadas','porcentaje_Ocupadas','numero_Desocupadas','porcentaje_Desocupadas','numero_Inactivas','porcentaje_Inactivas']
+    success_url = reverse_lazy('trabajo_app:trabajo')
+
+class modificarTablaDI(UpdateView):
+    template_name = 'trabajo/tipo-disc/crear.html'
+    model = tablaTipoDiscapacidad
+    fields = ['discapacidad','numero_Ocupadas','porcentaje_Ocupadas','numero_Desocupadas','porcentaje_Desocupadas','numero_Inactivas','porcentaje_Inactivas']
+    success_message = "El registro %(nombre)s fue eliminado exitosamente"
+    success_url = reverse_lazy('trabajo_app:trabajo')
+
+class eliminarTablaDI(DeleteView):
+    template_name = 'trabajo/tipo-disc/crear.html'
+    model = tablaTipoDiscapacidad
+    fields = ['discapacidad','numero_Ocupadas','porcentaje_Ocupadas','numero_Desocupadas','porcentaje_Desocupadas','numero_Inactivas','porcentaje_Inactivas']
+    success_message = "El registro %(nombre)s fue eliminado exitosamente"
+    success_url = reverse_lazy('trabajo_app:trabajo')
+
+# -------------------- CRUD DE TABLA CATEGORIA ---------------------------
+
+class crearTablaCA(CreateView):
+    template_name = 'trabajo/tabla-categoria/crear.html'
+    model = tablaCategoria
+    fields = ['categoria','hombre','mujer','total']
+    success_url = reverse_lazy('trabajo_app:trabajo')
+
+class modificarTablaCA(UpdateView):
+    template_name = 'trabajo/tabla-categoria/crear.html'
+    model = tablaCategoria
+    fields = ['categoria','hombre','mujer','total']
+    success_url = reverse_lazy('trabajo_app:trabajo')
+
+class eliminarTablaCA(DeleteView):
+    template_name = 'trabajo/tabla-categoria/crear.html'
+    model = tablaCategoria
+    fields = ['categoria','hombre','mujer','total']
+    success_message = "El registro %(nombre)s fue eliminado exitosamente"
+    success_url = reverse_lazy('trabajo_app:trabajo')
+
 
