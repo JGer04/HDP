b942d4e7ffab4c4ce23e5ceb6155144424986406
JGer04 <juarezalex282@gmail.com>
2023-06-13 10:55:23 -0600

    Cambios

--- Proyecto/Proyecto/settings.py
+++ Proyecto/Proyecto/settings.py
@@ -85,10 +85,22 @@ WSGI_APPLICATION = 'Proyecto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hdp',
+        'USER': 'admin',
+        'PASSWORD': 'Jo20004*',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
